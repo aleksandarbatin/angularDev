30fa036951862f39fd98608cd8505c742088e7c7
Aleksandar Batin <aleksandar.batin@diwanee.com>
2016-11-17 10:35:55 +0100

    inistial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+angular.module("root", [])
+    .controller("index", ["$scope", function($scope) {
+        $scope.favoriteWords;
+        $scope.favoriteMeal;
+        $scope.favoriteDrink;
+
+        $scope.value = 1;
+		$scope.isRed = function () { return $scope.value == 1; };
+        $scope.isBlue = function () { return $scope.value == 2; };
+        $scope.isYellow = function () { return $scope.value == 4; };
+        $scope.isGreen = function () { return $scope.value == 3; };
+
+        $scope.products = [
+			{id: 1, name: "Hockey puck"},
+			{id: 2, name: "Golf club"},
+			{id: 3, name: "Baseball bat"},
+			{id: 4, name: "Lacrosse stick"}
+		];
+		$scope.isFirstElementVisible = true;
+		$scope.isSecondElementVisible = true;
+		
+		$scope.message = "Hello World";
+    }]);
