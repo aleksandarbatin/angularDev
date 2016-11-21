5c7bf018a202f7d413c161ae2371f6ee5680698a
Aleksandar Batin <aleksandar.batin@diwanee.com>
2016-11-21 16:42:46 +0100

    likes dislikes

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
 	// dependencies
 
-angular.module("root", ["services"])
+var myApp = angular.module("root", ["services"]);
+    myApp.controller("index", ["$scope","hint", function($scope,hint) {
+    	var employees = [
+	    	{
+			name: "Pera",
+	    	forName: "Peric",
+	    	birthYear: "1859"
+	    	}, 
 
-.controller("index", ["$scope","hint", function($scope,hint) {
-    $scope.favoriteWords;
-    $scope.favoriteMeal;
-    $scope.favoriteDrink;
+	    	{
+			name: "Zika",
+	    	forName: "Lazic",
+	    	birthYear: "1955"
+	    	}
+		];
 
-    $scope.value = 1;
-	$scope.isRed = function () { return $scope.value == 1; };
-    $scope.isBlue = function () { return $scope.value == 2; };
-    $scope.isYellow = function () { return $scope.value == 4; };
-    $scope.isGreen = function () { return $scope.value == 3; };
+		$scope.employees = employees;
 
-    $scope.products = [
-		{id: 1, name: "Hockey puck"},
-		{id: 2, name: "Golf club"},
-		{id: 3, name: "Baseball bat"},
-		{id: 4, name: "Lacrosse stick"}
-	];
-	$scope.isFirstElementVisible = true;
-	$scope.isSecondElementVisible = true;
-	
-	$scope.message = "Hello World";
-	$scope.hint = hint;
-}]);
+		var tech = [
+	    	{name:"C#", likes: 0, dislikes: 0},
+	    	{name:"ASP.NET", likes: 0, dislikes: 0},
+	    	{name:"SQL Server", likes: 0, dislikes: 0},
+	    	{name:"AngularJS", likes: 0, dislikes: 0}
+    	];
+
+    	$scope.tech = tech; 
+    	$scope.plusLikes = 	function(tech){
+    		tech.likes++;	
+    	}
+
+    	$scope.plusDislikes = 	function(tech){
+    		tech.dislikes++;	
+    	}
+		
+        $scope.favoriteWords;
+        $scope.favoriteMeal;
+        $scope.favoriteDrink;
+
+        $scope.value = 1;
+		$scope.isRed = function () { return $scope.value == 1; };
+        $scope.isBlue = function () { return $scope.value == 2; };
+        $scope.isYellow = function () { return $scope.value == 4; };
+        $scope.isGreen = function () { return $scope.value == 3; };
+
+        $scope.products = [
+			{id: 1, name: "Hockey puck"},
+			{id: 2, name: "Golf club"},
+			{id: 3, name: "Baseball bat"},
+			{id: 4, name: "Lacrosse stick"}
+		];
+		$scope.isFirstElementVisible = true;
+		$scope.isSecondElementVisible = true;
+		
+		$scope.message = "Hello World";
+		$scope.hint = hint;
+    }]);
+
+    
 
     // value
 
